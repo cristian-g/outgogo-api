84892e70ca61964299b6c2fbbb7b953de10167a9
cristian-g <cristian@cristiangonzalez.com>
2019-08-18 20:52:28 +0200

    Removed fake api endpoints

--- routes/api.php
+++ routes/api.php
@@ -44,24 +44,3 @@ Route::put('/outgo/{id}', 'OutgoController@update')->middleware('jwt');
 Route::post('/vehicle/{vehicle_id}/payment', 'PaymentController@store')->middleware('jwt');
 Route::get('/payment/{id}', 'PaymentController@show')->middleware('jwt');
 Route::put('/payment/{id}', 'PaymentController@update')->middleware('jwt');
-
-// ------------------------------
-// Fake
-// ------------------------------
-Route::get('/fake0', 'VehicleController@fake0');
-Route::get('/fake1', 'VehicleController@fake1');
-Route::get('/fake2', 'VehicleController@fake2');
-Route::get('/fake3', 'VehicleController@fake3');
-Route::get('/signup1', 'VehicleController@signup1');
-Route::get('/signup2', 'VehicleController@signup2');
-Route::get('/signup3', 'VehicleController@signup3');
-
-// Reset database
-Route::get('reset-database', function (Request $request) {
-    //return shell_exec('php artisan migrate:rollback') . shell_exec('php artisan migrate');
-    \Illuminate\Support\Facades\DB::statement('SET FOREIGN_KEY_CHECKS = 0');
-    \Illuminate\Support\Facades\Artisan::call('migrate:rollback');
-    \Illuminate\Support\Facades\DB::statement('SET FOREIGN_KEY_CHECKS = 1');
-    \Illuminate\Support\Facades\Artisan::call('migrate');
-    return "Yey! Data has been reset. Go back to your tests, little grasshopper!";
-});
